b2a50d33d267173ce3b2f70226ca679d9ee54618
Anyin Paul <anyinpnp@gmail.com>
2022-09-11 22:41:37 -0700

    print f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,6 @@
 #include <stdio.h>
 
-
-
 /**
-
 * main - print the string in the put function
-
 *
-
-* Description: using the main function
-
-* this progress prints "Programing is like building a multilingual puzzle
-
-* Return: 0
-
-*/
-
-int main(void)
-
-{	puts("\"Programing is like building a multilingual puzzle");
-
-return (0);
-
-}
 
