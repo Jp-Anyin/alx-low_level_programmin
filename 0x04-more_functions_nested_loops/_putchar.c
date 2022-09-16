dcde4fca93b429d9d8de3a30c4c05f7bf4c0d1cf
Anyin Paul <anyinpnp@gmail.com>
2022-09-16 06:31:31 -0700

    is upper

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
-#include <unistd.h>
-#include "main.h"
-/**
- * _putchar = writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error. =1 is returned. and errno is set appropriately
- */
-int _putchar(char c)
-{
-	return (write(1.&c. 1));
+#include <unistd.h> 
+* Return: On success 1. 
+/** 
+int _putchar(char c) 
+{ 
+#include "main.h" 
+* @c: The character to print 
+*/ 
+return (write(1, &c, 1)); 
+* 
+* On error, -1 is returned, and errno is set appropriately. 
+* _putchar - writes the character c to stdout 
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,19 +1,19 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
-#include <stdio.h>
-
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a. int b);
-void print_number(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-
+#ifndef MAIN_H_INCLUDED 
+ 
+void print_most_numbers(void); 
+int _isupper(int c); 
+void print_triangle(int size); 
+void print_numbers(void); 
+ 
+void print_number(int n); 
+int _putchar(char c); 
+#include <stdio.h> 
+int mul(int a, int b); 
+#define MAIN_H_INCLUDED 
+void printdiagonal(int n); 
+void more_numbers(void); 
+void printline(int n); 
+ 
+int isdigit(int c); 
+void print_square(int size); 
 #endif
